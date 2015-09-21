0221c9b0252eb4dfa465eb0a9cd878f475dc45e2
ahmad luky <luky.lucky@yahoo.com>
2015-09-21 15:27:18 +0700

    init 2

--- src/mongodb/TweetDataMain.java
+++ src/mongodb/TweetDataMain.java
@@ -3,6 +3,7 @@ package mongodb;
 import java.net.UnknownHostException;
 
 // running library data
+// data mongodb
 public class TweetDataMain {
 	public static void main(String args[]) throws UnknownHostException
 	{
